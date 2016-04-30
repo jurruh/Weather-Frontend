01882333c3f608920704130e44c54a88d875f7e7
Jurre Vriesen <jurrevriesen@gmail.com>
2016-04-30 17:54:08 +0200

    Cards for temperature and humidity added

--- scripts/controllers/weatherController.js
+++ scripts/controllers/weatherController.js
@@ -1,3 +1,27 @@
-app.controller('weatherController', function($scope) {
-  $scope.latestTemperature = "32";
+app.controller('weatherController', function($scope, $http) {
+  $scope.cards = [];
+
+  $scope.init = function () {
+    $http.get("data.json")
+      .then(function(response) {
+          $scope.latestTemperature = response.data[response.data.length - 1].temperature;
+          $scope.latestHumidity = response.data[response.data.length - 1].humidity;
+
+          $scope.cards.push({
+            "title": "Temperature",
+            "value" : $scope.latestTemperature,
+            "unit" : "°C"
+          });
+
+          $scope.cards.push({
+            "title": "Humidity",
+            "value" : $scope.latestHumidity,
+            "unit" : "%"
+          });
+
+          $scope.latestHumidity = response.data[response.data.length - 1].humidity;
+      });
+  }
+
+  $scope.init();
 });
